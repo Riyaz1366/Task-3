af7780bc209b55e7c424d182c07ef25c91911fee
Riyaz1366 <mohammedriyaz337@gmail.com>
2023-01-26 23:44:17 +0530

    First Commit

---
+++ Resume.js
@@ -0,0 +1,102 @@
+    let resume ={
+  
+              "Name"          : "Riyaz Mohideen",
+              "MOBILE NO"     : "9677288962",
+              "ADDRESS"       : "NO.15, Anna Salai Periyar Nagar Alwarthirunagar, CHENNAI-600087",
+              "EMAIL ID"      : "mohammedriyaz337@gmail.com",
+              "JOB OBJECTIVE" : "To work in a firm with a professional work driven environment where I can utilize and apply my knowledge, skills which would enable me as a potential graduate to grow while fulfilling organizational goals.",
+                 
+          
+
+             "Experience" : [{
+             "Job Title"   : "Transaction Process Associate",
+             "Company"     : "Accenture Solutions Private ltd",
+             "Place"       : "Chennai, India", 
+             "From"        : "Nov 2016 (6 years)"
+
+             }], 
+
+             "Education" : [{
+             "QUALIFICATION": "Secondary SCHOOL (10TH)",
+             "INSTITUTION"  : "Sri Venkateshwara Matric Hr. Sec School Chennai-600087",
+             "YEAR"          : "2011",
+             "PERCENTAGE"    : "64.25%" 
+             },
+             {
+              "QUALIFICATION ": "HIGHER SECONDARY (12TH)",
+              "INSTITUTION "  : "Sri Venkateshwara Matric Hr. Sec School Chennai-600087",
+               "YEAR"         : "2013  ",
+               "PERCENTAGE"   : "62.4% "
+              },
+              {
+              "QUALIFICATION ": "BCA",
+              "INSTITUTION "  : "Madras University",
+              "YEAR"          :  "2016",
+              "PERCENTAGE"    : "58%" 
+                }],
+
+             " TECHNICAL SKILL " : [
+
+             " Office Tools: Functional with MS Office tools (Word, Excel & Power point) ",  
+
+             " Ability to self-educate appropriate software applications ", 
+              
+             "Interacting with clients related to the project.",  
+              
+             "Have Extensive knowledge on formulas like Vlook Up, Concatenate, Count if etc..", 
+             ],
+
+             "CERTIFICATIONS": [
+
+             "Completed 3 months course in .NET4.5 at Faaz Technology Solutions, Saidapet Chennai-15.",
+
+             "Attended Two Days Workshop on “Developing Real Time Application by Implementing 3tier Architecture is ASP.NET” at Faaz Technology Solutions, Saidapet, Chennai -15.",
+
+             "Completed Type Writing in Junior Level" 
+             ],
+             "PERSONAL SKILLS" :[
+
+             "Time management",   
+
+             "Work effectively in a team-oriented, collaborative environment.", 
+
+             "Good team player", 
+
+             "Efficient work and Good understanding skills",
+
+             ],
+
+             "Achievements" : [
+
+             "Awarded as New Bee in my Early career",
+
+            "Also Got Star of business Award two times.",
+
+             ],
+
+            "PERSONAL Details": { 
+
+            "Father NAME"            : "Mohammed Auyb ",
+             
+            "Mother Name"            : "Mehar Nisha ",
+             
+            "EMAIL-ID"               : "mohammedriyaz337@gmail.com",
+             
+            "Date of Birth"          : "16th Apr 1995  ",
+             
+            "GENDER"                 : "MALE",
+            
+            "NATIONALITY"            :  "Indian "  ,
+        
+            "Linguistic Versatility" :"Tamil, English  ",
+             
+           "Leisure Pursuits"  : "Playing shuttle, Watching movies"
+         },
+             
+        }
+        
+
+               
+                        
+
+                      
