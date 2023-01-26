eae6df0f9e51c8152661315d9f235e7f35a3ff69
Riyaz1366 <mohammedriyaz337@gmail.com>
2023-01-27 01:02:18 +0530

    fourth commit

--- For loop.js
+++ For loop.js
@@ -4,16 +4,16 @@ let Personaldata ={
     "MOBILE NO"     : "9677288962",
     "ADDRESS"       : "NO.15, Anna Salai Periyar Nagar Alwarthirunagar, CHENNAI-600087",
     "EMAIL ID"      : "mohammedriyaz337@gmail.com",
-    "Father NAME"   : "Mohammed Auyb ",
+    "Father Name"   : "Mohammed Auyb ",
     "Mother Name"   : "Mehar Nisha ",
     "Date of Birth" : "16th Apr 1995  ",
     "GENDER"        : "MALE",
 } 
 
-let keys = object.keys(personaldata);
+let keys = Object.Keys(Personaldata);
 
 for ( let i=0; i<keys.length; i++)
 {
 
-   console.log(i + ":" + personaldata[i]);
+   console.log(i + ":" +  Personaldata[i]);
 }
